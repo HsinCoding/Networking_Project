69d97222a019c3c39123071bd685a468a142f8e4
starlychen <starly.chen@car-plus.com.tw>
2019-09-02 09:29:35 +0800

    Initial Commit

---
+++ Networking_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking_Project
+//
+//  Created by ＵＳＥＲ on 2019/9/2.
+//  Copyright © 2019 hsin_project. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
